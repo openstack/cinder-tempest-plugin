2be6e107ae875f367f8443d5fc260856df17c99c
lkuchlan <lkuchlan@redhat.com>
2024-11-27 14:43:25 +0200

    Ensure server and backed volume are created in the same AZ
    This change ensures that the server and its associated backed volume
    are created in the same AZ, addressing scenarios where mismatched AZs
    could lead to operational failures or inconsistencies.
    
    Closes-Bug: #2089766
    
    Change-Id: Ibd33dbe024d9f82cd17bb072664ae56ce0604388

--- cinder_tempest_plugin/api/volume/base.py
+++ cinder_tempest_plugin/api/volume/base.py
@@ -91,6 +91,10 @@ class BaseVolumeTest(api_version_utils.BaseMicroversionTest,
             name = data_utils.rand_name(cls.__name__ + '-Volume')
             kwargs['name'] = name
 
+        if CONF.compute.compute_volume_common_az:
+            kwargs.setdefault('availability_zone',
+                              CONF.compute.compute_volume_common_az)
+
         volume = cls.volumes_client.create_volume(**kwargs)['volume']
         cls.addClassResourceCleanup(
             cls.volumes_client.wait_for_resource_deletion, volume['id'])
